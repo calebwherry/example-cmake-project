3fd32a4ec5b4f3437fba31730ddfe629bb362929
J. Caleb Wherry <caleb@calebwherry.com>
2015-04-02 23:35:17 -0400

    Removing clang compiler option of -stdlib=stdlibc++ since it fails on some systems. Detecting if libstdc++ is installed (instead of libc++) is something I will eventually look into but dont have the time right now.

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -29,13 +29,13 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   # Check version. If empty, warn. If too old, error out:
   if ("${CMAKE_CXX_COMPILER_VERSION}" STREQUAL "")
     message(WARNING "Clang compiler version is unknown, proceed at your own risk!")
-  elseif (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.6)
-    message(FATAL_ERROR "Clang compiler version must be at least 3.6 (current version: ${CMAKE_CXX_COMPILER_VERSION})!")
+  elseif (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.4)
+    message(FATAL_ERROR "Clang compiler version must be at least 3.4 (current version: ${CMAKE_CXX_COMPILER_VERSION})!")
   endif()
 
   # Set compiler/linker specific flags:
-  set(CMAKE_CXX_FLAGS_DEBUG "-Wall -ggdb -std=c++11 -stdlib=libc++")
-  set(CMAKE_CXX_FLAGS_RELEASE "-w -O3 -std=c++11 -stdlib=libc++")
+  set(CMAKE_CXX_FLAGS_DEBUG "-Wall -ggdb -std=c++11")
+  set(CMAKE_CXX_FLAGS_RELEASE "-w -O3 -std=c++11")
 
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 
