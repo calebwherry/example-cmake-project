047c894ea25c3df5fbb3c15ab1a18a5e92754446
J. Caleb Wherry <caleb@calebwherry.com>
2016-04-23 18:49:51 -0400

    Updates.

--- cmake_modules/CompilerOptions.cmake
+++ cmake_modules/CompilerOptions.cmake
@@ -13,29 +13,28 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
   # Check version. If empty, warn. If too old, error out:
   if ("${CMAKE_CXX_COMPILER_VERSION}" STREQUAL "")
     message(WARNING "GCC compiler version is unknown, proceed at your own risk!")
-  elseif (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.8)
-    message(FATAL_ERROR "GCC compiler version must be at least 4.8 (current version: ${CMAKE_CXX_COMPILER_VERSION})!")
+  elseif (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9)
+    message(FATAL_ERROR "GCC compiler version must be at least 4.9 (current version: ${CMAKE_CXX_COMPILER_VERSION})!")
   endif()
 
   # Set compiler/linker specific flags:
-  set(CMAKE_CXX_FLAGS_DEBUG "-Wall -ggdb -std=c++11")
-  set(CMAKE_CXX_FLAGS_RELEASE "-w -O3 -std=c++11")
-
-  # Warnings to ignore during C++ debug build:
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wno-format-extra-args")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+  #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
+  #set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
 
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 
   # Check version. If empty, warn. If too old, error out:
   if ("${CMAKE_CXX_COMPILER_VERSION}" STREQUAL "")
     message(WARNING "Clang compiler version is unknown, proceed at your own risk!")
-  elseif (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.4)
-    message(FATAL_ERROR "Clang compiler version must be at least 3.4 (current version: ${CMAKE_CXX_COMPILER_VERSION})!")
+  elseif (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.7)
+    message(FATAL_ERROR "Clang compiler version must be at least 3.7 (current version: ${CMAKE_CXX_COMPILER_VERSION})!")
   endif()
 
   # Set compiler/linker specific flags:
-  set(CMAKE_CXX_FLAGS_DEBUG "-Wall -ggdb -std=c++11")
-  set(CMAKE_CXX_FLAGS_RELEASE "-w -O3 -std=c++11")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+  #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
+  #set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
 
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 
@@ -47,9 +46,10 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   endif()
 
   # Set compiler/linker specific flags:
-  #   Note: Using defaults, overrride if need be.
-  #set(CMAKE_CXX_FLAGS_DEBUG "")
-  #set(CMAKE_CXX_FLAGS_RELEASE "")
+  # Note: Using defaults.
+  #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
+  #set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
 
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
 
@@ -61,9 +61,10 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
   endif()
 
   # Set compiler/linker specific flags:
-  #   Note: Using defaults, overrride if need be.
-  #set(CMAKE_CXX_FLAGS_DEBUG "")
-  #set(CMAKE_CXX_FLAGS_RELEASE "")
+  # Note: Using defaults.
+  #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
+  #set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
 
 else()
   message(WARNING "The '${CMAKE_CXX_COMPILER_ID}' compiler is untested and unsupported, continue at your own risk!")
