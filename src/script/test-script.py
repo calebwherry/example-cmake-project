1156e8aedb81d80d54bf3bb9558647bae83af3e5
J. Caleb Wherry <caleb@calebwherry.com>
2016-04-24 12:21:13 -0400

    Removing files.

--- src/script/test-script.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-########################################
-## \file    test-script.py
-## \date    01/01/2014
-## \brief   Test python script.
-########################################
-
-print("Hello World!")
